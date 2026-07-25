16e21e072725254846824024561d8014526fe597
subedihimal <itissubedi@gmail.com>
2026-07-25 17:44:15 +0545

    add dynamic UI for prompt entering

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,28 @@
 const input = document.querySelector('#input');
-
+const chatContainer = document.querySelector('#chatContainer')
 input.addEventListener('keyup', handleEnter)
+
+const askBTN = document.querySelector("#ask");
+ask.addEventListener('click', handleEnter);
+
+function generate(text){
+    // Append message to UI
+    // Send it to LLM
+    //Append response to the ui
+
+    const msg = document.createElement('div');
+    msg.className =`my-6 bg-neutral-800 p-3 rounded-xl ml-auto max-w-fit`;
+    msg.textContent = text;
+    chatContainer?.appendChild(msg);
+    input.value= ' ';
+}
+
 function handleEnter(e){
-    if (e.key == "Enter"){
+    if (e.key == "Enter" || e.type=="click"){
         const text = input?.value.trim();
         if(!text){
             return;
         }
-
-        console.log(text);
+        generate(text);
     }
 }
